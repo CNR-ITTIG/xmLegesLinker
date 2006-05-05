a08fb6b84363391f99e7ff88fca2cbf0c1617518
Pierluigi Spinosa <pierluigi.spinosa@ittig.cnr.it>
2006-05-05 14:57:34 +0000

    aggiunti parametri x rif. non completi o interni
    git-svn-id: https://svn.ittig.cnr.it/svn/xmLegesLinker/trunk@21 c2e715ce-4917-0410-8b6a-fb915d9de5a5

--- uscita.h
+++ uscita.h
@@ -1,6 +1,6 @@
 /******************************************************************************
 * Project:	xmLeges
-* Module:	Linker
+* Module:		Linker
 * File:		uscita.h
 * Copyright:	ITTIG/CNR - Firenze - Italy (http://www.ittig.cnr.it)
 * Licence:	GNU/GPL (http://www.gnu.org/licenses/gpl.html)
@@ -13,8 +13,8 @@
 #include "urn.h"
 
 void uscitaLista(urn *u[], int n);
-void uscitaListaConTesto(char *buf, urn *u[], int n);
-void uscitaInserimento(char *buf, urn *u[], int n, char *prima, char *dopo);
-
+void uscitaListaConTesto(char *buf, urn *u[], int n, int noc, char *nocprima, char *nocdopo );
+void uscitaInserimento(char *buf, urn *u[], int n, char *prima, char *dopo, int noc, char *nocprima, char *nocdopo);
+void uscitaMascheraRif(char *buf, urn *u[], int n);
 
 #endif /* __USCITA_H__ */
