e0d71d1c47aa42eaf838fb945aa04b0a68305fd3
Pierluigi Spinosa <pierluigi.spinosa@ittig.cnr.it>
2006-05-05 14:52:58 +0000

    aggiunta uscita per rfi. non completi e interni
    git-svn-id: https://svn.ittig.cnr.it/svn/xmLegesLinker/trunk@19 c2e715ce-4917-0410-8b6a-fb915d9de5a5

--- uscita.c
+++ uscita.c
@@ -1,29 +1,32 @@
 /******************************************************************************
 * Project:	xmLeges
-* Module:	Linker
+* Module:		Linker
 * File:		uscita.c
 * Copyright:	ITTIG/CNR - Firenze - Italy (http://www.ittig.cnr.it)
 * Licence:	GNU/GPL (http://www.gnu.org/licenses/gpl.html)
 * Authors:	Mirco Taddei (m.taddei@ittig.cnr.it)
+*			PierLuigi Spinosa (pierluigi.spinosa@ittig.cnr.it)
 ******************************************************************************/
 
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <IttigUtil.h>
 #include "config.h"
 #include "uscita.h"
-#include <IttigUtil.h>
 #include "urn.h"
 
 void uscitaOneSpace(int c, int *spazio);
 
-char * uscitaCalcolaStringa(char *str, urn *u) {
+char * uscitaCalcolaStringa(char *str, urn *u) 
+{
 	char *s;
 	char * t;
 	char * r = NULL;
 
 	s = strdup(str);
-	while ((t = strstr(s, "__URN__"))) {
+	while ((t = strstr(s, "__URN__"))) 
+	{
 		*t = 0;
 		r = utilConcatena(3, r, s, urnStringa(*u));
 		*t = '_';
@@ -35,25 +38,48 @@ char * uscitaCalcolaStringa(char *str, urn *u) {
 
 
 
-void uscitaDaFile(char *buf, urn *u[], int n, char *prima, char *dopo, int tutto, int nl) {
+void uscitaMascheraRif(char *buf, urn *u[], int n) 
+{
+	register int i = 0;
+	register int j = 0;
+
+	for (j = 0; j < n; j++)
+	{
+		for (i = u[j]->inizio; i < u[j]->fine; i++) 
+		{
+			*(buf + i) = '#';
+		}
+	}
+}
+
+
+
+void uscitaDaFile(char *buf, urn *u[], int n, char *prima, char *dopo, int tutto, int nl, int noc, char* nocprima, char* nocdopo) 
+{
 	register int i = 0;
 	register int j = 0;
 	register int blocco = 0;
 	int spazio = 1;
 	int tag = 0;
 
-	for (i = 0; *buf; i++, buf++) {
+	for (i = 0; *buf; i++, buf++) 
+	{
 		if (j<n)
-			if (i == u[j]->inizio) {
-				printf("%s", uscitaCalcolaStringa(prima, u[j]));
+			if (i == u[j]->inizio) 
+			{
+				if (u[j]->tipo != 'n') 			printf("%s", uscitaCalcolaStringa(prima, u[j]));
+				if (u[j]->tipo == 'n' && noc) printf("%s", uscitaCalcolaStringa(nocprima, u[j]));
 				blocco = 1;
 			}
-		if (blocco || tutto) {
+		if (blocco || tutto) 
+		{
 			int c = *buf;
 			if (nl)				// duplica input
 				putchar(c);
-			else {				// toglie spazi e tag
-				if (configGetTipoInput() != txt) {	// html o xml
+			else 
+			{				// toglie spazi e tag
+				if (configGetTipoInput() != txt)		//urnStringa(*u) html o xml
+				{
 					if (c == '<') tag = 1;
 					if (!tag) uscitaOneSpace(c, &spazio);
 					if (c == '>') tag = 0;
@@ -62,8 +88,10 @@ void uscitaDaFile(char *buf, urn *u[], int n, char *prima, char *dopo, int tutto
 			}
 		}
 		if (j < n && blocco)
-			if (i == u[j]->fine-1) {
-				printf("%s", uscitaCalcolaStringa(dopo, u[j]));
+			if (i == u[j]->fine-1) 
+			{
+				if (u[j]->tipo != 'n') 			printf("%s", uscitaCalcolaStringa(dopo, u[j]));
+				if (u[j]->tipo == 'n' && noc) printf("%s", uscitaCalcolaStringa(nocdopo, u[j]));
 				j++;
 				blocco = 0;
 			}
@@ -72,13 +100,16 @@ void uscitaDaFile(char *buf, urn *u[], int n, char *prima, char *dopo, int tutto
 
 
 
-void uscitaOneSpace(int c, int *spazio) {
+void uscitaOneSpace(int c, int *spazio) 
+{
 
-	if (!isspace(c)) {
+	if (!isspace(c)) 
+	{
 		putchar(c);
 		*spazio = 0;
 	}
-	else if (!*spazio) {
+	else if (!*spazio) 
+	{
 		putchar(' ');
 		*spazio = 1;
 	}
@@ -86,22 +117,25 @@ void uscitaOneSpace(int c, int *spazio) {
 
 
 
-void uscitaLista(urn *u[], int n) {
+void uscitaLista(urn *u[], int n) 
+{
 	register int i;
 
 	for (i=0; i < n; i++)
-		printf("%-60s\t%6ld\t%6ld\n", urnStringa(*(u[i])), u[i]->inizio, u[i]->fine);
+		printf("%s1 %-60s\t%6ld\t%6ld\n", u[i]->tipo, urnStringa(*(u[i])), u[i]->inizio, u[i]->fine);
 }
 
 
 
-void uscitaListaConTesto(char *buf, urn *u[], int n) {
-	uscitaDaFile(buf, u, n, "__URN__\t", "\n", 0, 0);
+void uscitaListaConTesto(char *buf, urn *u[], int n, int noc, char *nocprima, char *nocdopo ) 
+{
+	uscitaDaFile(buf, u, n, "__URN__\t", "\n", 0, 0, noc, "?__URN__?\t", "\n");
 }
 
 
 
-void uscitaInserimento(char *buf, urn *u[], int n, char *prima, char *dopo) {
-	uscitaDaFile(buf, u, n, prima, dopo, 1, 1);
+void uscitaInserimento(char *buf, urn *u[], int n, char *prima, char *dopo, int noc, char *nocprima, char *nocdopo ) 
+{
+	uscitaDaFile(buf, u, n, prima, dopo, 1, 1, noc, nocprima, nocdopo);
 }
 
